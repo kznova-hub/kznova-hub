94dda17c6021c0d8e4e26ecf425303436b9ab9c3
kznova-hub <jigitjeken16@gmail.com>
2026-03-20 10:49:14 +0200

    Update myhub.lua

--- myhub.lua
+++ myhub.lua
@@ -1,52 +1,26 @@
+repeat wait() until game:IsLoaded()
+
+local success, err = pcall(function()
+
 getgenv().Settings = {
     ["Raid Settings"] = {
         Enabled = true,
-        Difficulty = 5000,
-        ["Boss Settings"] = {
-            Enabled = true,
-        },
-        ["Egg Settings"] = {
-            Enabled = true,
-            MinimumEggMulti = 500,
-            MinimumLuckyCoins = "1m",
-        },
+        Difficulty = 5000
     }
 }
-
-if game.CoreGui:FindFirstChild("Rayfield") then
-    game.CoreGui.Rayfield:Destroy()
-end
 
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "KZNOVA HUB",
    LoadingTitle = "KZNOVA HUB",
-   LoadingSubtitle = "by you",
+   LoadingSubtitle = "Loading...",
    ConfigurationSaving = {
       Enabled = false
    }
 })
 
-local Tab = Window:CreateTab("Main", 4483362458)
-
-Tab:CreateToggle({
-   Name = "Enable Raid",
-   CurrentValue = true,
-   Callback = function(Value)
-       getgenv().Settings["Raid Settings"].Enabled = Value
-   end,
-})
-
-Tab:CreateSlider({
-   Name = "Difficulty",
-   Range = {0, 10000},
-   Increment = 100,
-   CurrentValue = 5000,
-   Callback = function(Value)
-       getgenv().Settings["Raid Settings"].Difficulty = Value
-   end,
-})
+local Tab = Window:CreateTab("Main")
 
 Tab:CreateButton({
    Name = "Start Script",
@@ -54,3 +28,9 @@ Tab:CreateButton({
        loadstring(game:HttpGet("https://raw.githubusercontent.com/tianhainrk/ps99/refs/heads/main/ps99.lua"))()
    end,
 })
+
+end)
+
+if not success then
+    warn(err)
+end
